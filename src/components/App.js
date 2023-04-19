f1e6b0373de13393d0e6e00d83ea69dd67a8b6f0
Sanechek <gilev6800@mail.ru>
2023-04-19 15:59:22 +0300

    Рабочая версия агрегатора фильмов

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,105 @@
-import logo from '../logo.svg';
-import '../App.css';
-import Header from './Header'
+import React, { useReducer, useEffect } from "react";
+import "../App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
 
-function App() {
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=e505899b";
+
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+
+    fetch(MOVIE_API_URL)
+        .then(response => response.json())
+        .then(jsonResponse => {
+
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+          });
+        });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=e505899b`)
+        .then(response => response.json())
+        .then(jsonResponse => {
+          if (jsonResponse.Response === "True") {
+            dispatch({
+              type: "SEARCH_MOVIES_SUCCESS",
+              payload: jsonResponse.Search
+            });
+          } else {
+            dispatch({
+              type: "SEARCH_MOVIES_FAILURE",
+              error: jsonResponse.Error
+            });
+          }
+        });
+  };
+
+  const { movies, errorMessage, loading } = state;
+
   return (
-    <Header />
+      <div className="App">
+        <Header text="HOOKED" />
+        <Search search={search} />
+        <p className="App-intro">Sharing a few of our favourite movies</p>
+        <div className="movies">
+          {loading && !errorMessage ? (
+              <span>loading... </span>
+          ) : errorMessage ? (
+              <div className="errorMessage">{errorMessage}</div>
+          ) : (
+              movies && movies.map((movie, index) => (
+                  <Movie key={`${index}-${movie.Title}`} movie={movie} />
+              ))
+          )}
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,4 +5,28 @@ const Search = (props) => {
   const handleSearchInputChanges = (e) => {
       setSearchValue(e.target.value);
   }
+
+  const resetInputField = () => {
+      setSearchValue("");
+  }
+
+  const callSearchFunction = (e) =>{
+      e.preventDefault();
+      props.search(searchValue);
+      resetInputField();
+  }
+
+  return (
+        <form className="search">
+            <input
+                value={searchValue}
+                onChange={handleSearchInputChanges}
+                type="text"
+            />
+            <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+        </form>
+  );
+
 };
+
+export default Search;
